b20050a5f5f0758fe0e20ec576ab8e5a2514e9b9
Sam Sheppe <ssheppe63@gmail.com>
2017-03-09 13:28:01 -0500

    moving background only on home page

--- code_3.js
+++ code_3.js
@@ -22,13 +22,13 @@ var images = ['bahai.jpg', 'barcelona.jpg', 'budapest.JPG', 'budapest2.jpg']
 // })
 // $('body').css({'background': 'url(carousel-images/' + images[Math.floor(Math.random() * images.length)] + ')'});
 
-// $(function(){
-// 	var x = 0;
-// 	setInterval(function(){
-// 		x-=1;
-// 		$('body').css('background-position', x + 'px 0');
-// 	}, 10);
-// })
+$(function(){
+	var x = 0;
+	setInterval(function(){
+		x-=1;
+		$('body.home').css('background-position', x + 'px 0');
+	}, 10);
+})
 
 
 
