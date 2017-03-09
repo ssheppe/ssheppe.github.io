2461254e9fe1bc36dd34b2e1691439dbaff06bea
Sam Sheppe <ssheppe63@gmail.com>
2017-03-09 13:24:00 -0500

    week 5 project and scrolling find out more hopefully working offscreen

--- code_3.js
+++ code_3.js
@@ -2,6 +2,8 @@
 var navBar = document.getElementById("spongebob-nav");
 var article = document.getElementById("spongebob-article")
 
+
+
 var toggleBtns = Array.from(document.getElementsByClassName("nav-toggle"));
 
 toggleBtns.forEach(function(btn) {
@@ -20,10 +22,35 @@ var images = ['bahai.jpg', 'barcelona.jpg', 'budapest.JPG', 'budapest2.jpg']
 // })
 // $('body').css({'background': 'url(carousel-images/' + images[Math.floor(Math.random() * images.length)] + ')'});
 
-$(function(){
-	var x = 0;
-	setInterval(function(){
-		x-=1;
-		$('body').css('background-position', x + 'px 0');
-	}, 10);
-})
+// $(function(){
+// 	var x = 0;
+// 	setInterval(function(){
+// 		x-=1;
+// 		$('body').css('background-position', x + 'px 0');
+// 	}, 10);
+// })
+
+
+
+$(".basic-info").click(function() {
+    $('body').animate({
+        scrollTop: $(".more_info").offset().top},
+        'slow');
+});
+
+ $("#more").on('click', function(e) {
+
+       // prevent default anchor click behavior
+       e.preventDefault();
+
+       // animate
+       $('div').animate({
+           scrollTop: $(this.hash).offset().top
+         }, 300, function(){
+   
+           // when done, add hash to url
+           // (default click behaviour)
+           window.location.hash = this.hash;
+         });
+
+    });
